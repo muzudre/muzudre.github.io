5a1b6fe3c12052e58f2f1c91b46b788e3f9e21d2
muzudre <muzudre@gmail.com>
2018-10-11 13:20:59 +0800

    Updates in mozghub

--- mozghub/assets/js/util.js
+++ mozghub/assets/js/util.js
@@ -1,10 +1,12 @@
-
 //  - https://github.com/theme-next/hexo-theme-next/blob/master/source/js/src/utils.js
 class utils {
   static getContentVisibilityHeight() {
-    var docHeight = $('.visible').height(),
+    var docHeight = $(".visible").height(),
       winHeight = $(window).height(),
-      contentVisibilityHeight = (docHeight > winHeight) ? (docHeight - winHeight) : ($(document).height() - winHeight);
+      contentVisibilityHeight =
+        docHeight > winHeight
+          ? docHeight - winHeight
+          : $(document).height() - winHeight;
     return contentVisibilityHeight;
   }
 
